f8b953d9c673dfc6110e1a6dd2e18305033a46e2
Jay <141212189+jayx2u@users.noreply.github.com>
2025-07-14 00:55:08 -0700

    manual: Clarify warning for BAB module

--- manual-web/src/app/page.tsx
+++ manual-web/src/app/page.tsx
@@ -291,7 +291,7 @@ export default function Home() {
           <div className="space-y-4">
             <div className="bg-yellow-100 border-l-4 border-yellow-500 text-yellow-800 p-4" role="alert">
               <p className="font-bold">This module can only be disarmed when all other modules (except needy ones) have been disarmed.</p>
-              <p>Failure to adhere to this rule will result in detonation.</p>
+              <p>Failure to adhere to this rule will result in immediate detonation.</p>
             </div>
             <p>
               This module features a single, large button which contains an LED that can be lit. The button has no text on it. To disarm it, the button may need to be pressed and held. The timing of the release is critical and depends on whether the LED is lit and other bomb features.
